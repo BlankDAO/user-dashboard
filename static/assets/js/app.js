f538f033310ba637866a5cc8e6aa562db3ba0c74
hrzp <hamidreza.num1@gmail.com>
2019-02-25 12:22:27 +0330

    add bright id section

--- static/assets/js/app.js
+++ static/assets/js/app.js
@@ -129,19 +129,7 @@ Alert = (function(){
 const routes = [
   { path: '/', component: httpVueLoader('components/home.vue') },
   { path: '/referrer', component: httpVueLoader('components/referrer.vue') },
-  { path: '/admin', component: httpVueLoader('components/admin.vue') },
-  { path: '/admin/dashboard', component: httpVueLoader('components/admin.vue') },
-  { path: '/admin/2fa', component: httpVueLoader('components/admin_components/2fa.vue') },
-  { path: '/admin/dashboard/wallet-managment/:id', component: httpVueLoader('components/admin_components/walletManagement.vue') },
-  { path: '/admin/dashboard/log-report', component: httpVueLoader('components/admin_components/logs.vue') },
-  { path: '/admin/dashboard/login-report', component: httpVueLoader('components/admin_components/loginHistory.vue') },
-  { path: '/admin/dashboard/role-managment', component: httpVueLoader('components/admin_components/roleManagment.vue') },
-  { path: '/admin/dashboard/role-managment/:id', component: httpVueLoader('components/admin_components/roleManagment.vue') },
-  { path: '/admin/dashboard/user-managment', component: httpVueLoader('components/admin_components/userManagment.vue') },
-  { path: '/admin/dashboard/shutdown-api', component: httpVueLoader('components/admin_components/shutdownApi.vue') },
-  { path: '/user/mail-verification', component: httpVueLoader('components/common/mailVerification.vue') },
-  { path: '/user/change-password', component: httpVueLoader('components/common/changePassword.vue') },
-  { path: '/payment', component: httpVueLoader('components/payment.vue') },
+  { path: '/bright-id', component: httpVueLoader('components/bright-id.vue') },
 ]
 
 const router = new VueRouter({
---
+++ static/components/bright-id.vue
@@ -0,0 +1,104 @@
+<template lang="html">
+<div class="container">
+  <div class="row justify-content-center">
+      <div class="col col-12">
+        <p class="title-head">Please scan this code in your brightID</p>
+      </div>
+      <div class="col col-12 row justify-content-center" id="qr">
+      </div>
+  </div>
+</div>
+</template>
+
+<style lang="css">
+.title-head {
+  font-family: proxima-regular;
+  font-size: 2em;
+  text-align: center;
+}
+.container {
+  padding: 1em 3em 2em 3em;
+  margin: 6em auto;
+  background-color: #fff;
+  border-radius: 4.2px;
+  box-shadow: 0px 3px 10px 2px rgba(0, 0, 0, 0.2);
+}
+</style>
+
+<script>
+  module.exports = {
+    data: function() {
+      return {
+        confrim: false,
+        server: 'http://127.0.0.1:3000',
+        msg: '',
+        qrcode: null,
+        cronjob: null,
+      }
+    },
+    props: [
+    ],
+    methods: {
+      init() {
+        this.qrcode = new QRCode("qr", {
+            text: "Nothing",
+            width: 255,
+            height: 255,
+            colorDark : "#000000",
+            colorLight : "#ffffff",
+            correctLevel : QRCode.CorrectLevel.H
+        });
+        this.newCode();
+        let myApp = this;
+        this.cronjob = setInterval(function(){
+          myApp.newCode();
+        }, 1000 * 60 * 1);
+      },
+      newCode() {
+        this.$http.get(this.server + '/new-code').then(function(response){
+          console.log(response);
+          let data = response.body;
+          if ( !data.status ) {
+            this.msg = data.msg + ' Your Referrer is: ' + data.args[0].referrer;
+            return;
+          }
+
+          this.qrcode.clear();
+          this.qrcode.makeCode(data.qr);
+          this.codeStatus(data.uuid, data.ae);
+        },function(response){
+          console.error('Error in Connection: ', response)
+        })
+      },
+      codeStatus(uuid, ae) {
+        let data = {
+          uuid: uuid,
+          ae: ae
+        }
+        let myApp = this;
+        this.$http.post(this.server + '/check-code', data).then(function(response){
+          let data = response.body;
+          console.log('status', data.status);
+          if ( !data.status ) {
+            setTimeout(function(){
+              myApp.codeStatus(uuid, ae);
+            }, 3000);
+            return;
+          }
+          clearInterval(this.cronjob);
+          Swal.fire(
+            'Done Successfully',
+            '',
+            'success'
+          );
+          console.log('success data', data.status);
+        },function(response){
+          console.error('Error in Connection: ', response)
+        });
+      }
+    },
+    mounted(){
+      this.init();
+    }
+  }
+</script>
