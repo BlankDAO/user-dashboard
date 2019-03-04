5599911388694cd33774b5f62b9a719451b1a2eb
siftal <m.khanmohamadzadeh@gmail.com>
2019-03-04 12:21:26 +0330

    Check member and signature before submit new user

--- server.py
+++ server.py
@@ -4,14 +4,13 @@ from flask import Flask, redirect, request, g, session
 from web3 import Web3, HTTPProvider
 from datetime import timedelta
 from io import BytesIO as IO
-from time import time as now
 from config import api_key
 from hashlib import sha256
+import nacl.encoding
+import nacl.signing
 import pymongo
 import config
-import uuid
 import gzip
-from pprint import pprint as pp
 import json
 import os
 
@@ -22,8 +21,9 @@ abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
 
+
 def check_api_key(_api_key, timestamp):
-    key = str(timestamp) +  ' - ' + api_key
+    key = str(timestamp) + ' - ' + api_key
     print(sha256(key.encode('utf-8')).hexdigest())
     if sha256(key.encode('utf-8')).hexdigest() == _api_key:
         return True
@@ -66,8 +66,8 @@ def error_to_client(error):
 def before_request():
     session.permanent = True
     app.permanent_session_lifetime = timedelta(minutes=10)
-    client = pymongo.MongoClient("mongodb://localhost:27017/")
-    g.db = client["blankdao"]
+    client = pymongo.MongoClient('mongodb://localhost:27017/')
+    g.db = client['blankdao']
     g.w3 = Web3(HTTPProvider(config.INFURA_URL))
 
 
@@ -97,32 +97,37 @@ def index():
 def get_info():
     data = json.loads(request.data)
     account = check_eth_addr(data['account'])
-    res = g.db.member.find_one({"account": account})
+    res = g.db.member.find_one({'account': account})
     print(type(res))
     if res:
         del res['_id']
-        pp(res)
-        return json.dumps({'status': True, "data": res, "brightid_confirm": True})
-
-    return json.dumps({
-        'status': True,
-        "data": {
-            "brightid_confirm": False
-        }
-    })
+        return json.dumps({
+            'status': True,
+            'data': res,
+            'brightid_confirm': True
+        })
+    return json.dumps({'status': True, 'data': {'brightid_confirm': False}})
 
 
 @app.route('/submit-member', methods=['POST'])
 def submit_member():
     data = json.loads(request.data)
-
-    data['points'] = 33
-    data['credit'] = 44
-    data['earned'] = 10
-    data['brightid_score'] = 80
+    if not verify_message(data['publicKey'], data['timestamp'], data['signedMessage']):
+        return json.dumps({
+            'status': False,
+            'message': 'Invalid data'
+        })
+    data['points'] = 0
+    data['credit'] = 0
+    data['earned'] = 0
+    data['brightid_score'] = brightid_score()
     data['account'] = check_eth_addr(data['account'])
+    if not g.db.member.find_one(data['account']):
+        return json.dumps({
+            'status': False,
+            'message': 'Already exists'
+        })
     g.db.member.insert_one(data)
-
     return json.dumps({
         'status': True,
     })
@@ -132,7 +137,7 @@ def submit_member():
 def check_account():
     data = json.loads(request.data)
     account = check_eth_addr(data['account'])
-    res = g.db.referrers.find_one({"account": account})
+    res = g.db.referrers.find_one({'account': account})
 
     if res:
         if res['registered']:
@@ -147,7 +152,7 @@ def add_referrer():
 
     referrer = check_eth_addr(data['referrer'])
     account = check_eth_addr(data['account'])
-    res = g.db.referrers.find_one({"account": account})
+    res = g.db.referrers.find_one({'account': account})
     if res:
         if res['registered']:
             raise ErrorToClient('Your account has already been registered')
@@ -161,10 +166,10 @@ def add_referrer():
                                       upsert=False)
     else:
         g.db.referrers.insert_one({
-            "referrer": referrer,
-            "account": account,
-            "hash": data["hash"],
-            "registered": False
+            'referrer': referrer,
+            'account': account,
+            'hash': data['hash'],
+            'registered': False
         })
     return json.dumps({
         'msg': 'Your Address Submied Successfully',
@@ -176,7 +181,7 @@ def add_referrer():
 def get_referrer():
     data = json.loads(request.data)
     account = check_eth_addr(data['account'])
-    doc = g.db.referrers.find({"account": account})
+    doc = g.db.referrers.find({'account': account})
     if doc:
         if doc['registered']:
             return json.dumps({'referrer': doc['referrer'], 'status': True})
@@ -188,7 +193,7 @@ def get_referred_investors():
     referred_investors = []
     data = json.loads(request.data)
     account = check_eth_addr(data['account'])
-    docs = g.db.referrers.find({"referrer": account})
+    docs = g.db.referrers.find({'referrer': account})
     for doc in docs:
         if doc['registered']:
             referred_investors.append(doc['account'])
@@ -199,26 +204,49 @@ def get_referred_investors():
 
 
 client = {
-    'client_id'      : '',
-    'client_secret'  : '',
-    'grant_type'     : 'authorization_code',
-    'redirect_uri'   : 'http://104.207.144.107:8000/instagram-auth',
-    'auth_uri'       : 'https://api.instagram.com/oauth/authorize/',
-    'token_uri'      : 'https://api.instagram.com/oauth/access_token/'
+    'client_id': '',
+    'client_secret': '',
+    'grant_type': 'authorization_code',
+    'redirect_uri': 'http://104.207.144.107:8000/instagram-auth',
+    'auth_uri': 'https://api.instagram.com/oauth/authorize/',
+    'token_uri': 'https://api.instagram.com/oauth/access_token/'
 }
+
 
 @app.route('/instagram-login')
 def instagram_login():
-    uri = client['auth_uri'] + '?client_id={0}&redirect_uri={1}&response_type=code'.format(client['client_id'], client['redirect_uri'])
+    uri = client[
+        'auth_uri'] + '?client_id={0}&redirect_uri={1}&response_type=code'.format(
+            client['client_id'], client['redirect_uri'])
     return flask.redirect(uri)
+
 
 @app.route('/instagram-auth')
 def auth():
     client.update({'code': flask.request.args.get('code')})
-    return flask.Response(requests.post(client['token_uri'], data=client).text, status=200, mimetype='application/json')
+    return flask.Response(
+        requests.post(client['token_uri'], data=client).text,
+        status=200,
+        mimetype='application/json')
     # resualt is:
-        # {"access_token": "6253355905.0cfde61.4099f3d082a74dabac30271d5e66c840", "user": {"id": "6253355905", "username": "hamidreza.zarepour", "profile_picture": "https://scontent.cdninstagram.com/vp/d5e9b26ffb35bca808a045fab87b55d3/5D24FE78/t51.2885-19/s150x150/45880528_202374717369019_3973935739212660736_n.jpg?_nc_ht=scontent.cdninstagram.com", "full_name": "Hamid reza zare pour", "bio": "Computer programmer.music lover", "website": "http://hrzp.lsbits.com/", "is_business": false}}
+    # {'access_token': '6253355905.0cfde61.4099f3d082a74dabac30271d5e66c840', 'user': {'id': '6253355905', 'username': 'hamidreza.zarepour', 'profile_picture': 'https://scontent.cdninstagram.com/vp/d5e9b26ffb35bca808a045fab87b55d3/5D24FE78/t51.2885-19/s150x150/45880528_202374717369019_3973935739212660736_n.jpg?_nc_ht=scontent.cdninstagram.com', 'full_name': 'Hamid reza zare pour', 'bio': 'Computer programmer.music lover', 'website': 'http://hrzp.lsbits.com/', 'is_business': false}}
+
+
+def brightid_score():
+    # TODO get scro from BrightID API.
+    return 0
+
+
+def verify_message(public_key, message, sig):
+    try:
+        verify_key = nacl.signing.VerifyKey(
+            public_key, encoder=nacl.encoding.URLSafeBase64Encoder)
+        encoder = nacl.encoding.URLSafeBase64Encoder
+        verify_key.verify(message, encoder.decode(sig))
+        return True
+    except:
+        return False
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0' , port=5008, threaded=True)
+    app.run(debug=True, host='0.0.0.0', port=5008, threaded=True)
