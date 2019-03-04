4e69f25ba264a0fa26c7ba15deed83343d3bd7b6
hrzp <hamidreza.num1@gmail.com>
2019-03-04 15:23:48 +0330

    update home ui - add social login in ui

--- static/components/home.vue
+++ static/components/home.vue
@@ -3,65 +3,128 @@
       <div class="row justify-content-center">
 	      <h2 class="col col-12">Dashboard</h2>
 	      <hr>
-	      <div class="col col-12 profile-img">
+	      <div class="col col-12 profile">
 	      	<img :src="$root.accountInfo.data.photo">
 	      </div>
+	      <div class="col col-12 name">
+	      	<p>{{$root.accountInfo.data.name}}</p>
+	      </div>
+	      <hr>
 	      <div class="col row col-sm-12 col-md-12 col-lg-4 box">
-	      	<div class="item-icon col col-8"><span>BlankDAO Point:</span></div>
-	      	<div class="col col-4 row">
+	      	<div class="item col col-6"><span>BlankDAO Point:</span></div>
+	      	<div class="col col-6 row">
 	      		<div class="value col col-6">{{$root.accountInfo.data.points}}</div>
 	      	</div>
 	      </div>
 	      <div class="col row col-sm-12 col-md-12 col-lg-4 offset-lg-2 box">
-	      	<div class="item-icon col col-8"><span>BrightId Score:</span></div>
-	      	<div class="col col-4 row">
+	      	<div class="item col col-6"><span>BrightId Score:</span></div>
+	      	<div class="col col-6 row">
 	      		<div class="value col col-6">{{$root.accountInfo.data.brightid_score}}</div>
 	      	</div>
 	      </div>
 	      <div class="col row col-sm-12 col-md-12 col-lg-4  box">
-	      	<div class="item-icon col col-8"><span>Credit:</span></div>
-	      	<div class="col col-4 row">
+	      	<div class="item col col-6"><span>Credit:</span></div>
+	      	<div class="col col-6 row">
 	      		<div class="value col col-6">{{$root.accountInfo.data.credit}}$</div>
 	      	</div>
 	      </div>
 	      <div class="col row col-sm-12 col-md-12 col-lg-4 offset-lg-2 box">
-	      	<div class="item-icon col col-8"><span>Earned:</span></div>
-	      	<div class="col col-4 row">
+	      	<div class="item col col-6"><span>Earned:</span></div>
+	      	<div class="col col-6 row">
 	      		<div class="value col col-6">{{$root.accountInfo.data.earned}}$</div>
 	      	</div>
 	      </div>
+      </div>
+      <hr>
+      <div class="row justify-content-center" style="margin-top: 5%;">
+      	<div class="col col-sm-12 col-md-12 col-lg-3" v-if="$root.accountInfo.data.instagram_auth">
+			<a class="btn btn-block btn-social ">
+	        	<i class="fab fa-instagram"></i> Sign in with Instagram
+	        </a>
+      	</div>
+      	<div class="col col-sm-12 col-md-12 col-lg-3" v-if="$root.accountInfo.data.twitter_auth">
+			<a class="btn btn-block btn-social ">
+	        	<i class="fab fa-twitter"></i> Sign in with Twitter
+	        </a>
+      	</div>
       </div>
     </div>
 
 </template>
 
 <style lang="css">
-.profile-img {
+
+.btn-social {
+    color: #000 !important;
+    background-color: #fff;
+    border-color: rgba(0,0,0,0.2);
+    position: relative;
+    padding-left: 44px;
+    text-align: left;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    margin-bottom: 3%;
+    transition: all ease 1s;
+}
+.btn-social:hover {
+	color: white !important;
+	background-color: #000;
+	transition: all ease 1s;
+}
+.btn-block {
+    display: block;
+    width: 100%;
+}
+.btn-social i {
+    position: absolute;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    width: 32px;
+    line-height: 34px;
+    font-size: 1.6em;
+    text-align: center;
+    border-right: 1px solid rgba(0,0,0,0.2);
+}
+.profile {
 	left: 43%;
 	margin: auto;
 }
-.profile-img img {
+.profile img {
 	-webkit-filter: grayscale(100%);
 	filter: grayscale(100%);
 	border-radius: 100px;
 	box-shadow: 0px 0px 10px 0 #000000;
 }
+.name {
+	font-size: 4vmin;
+	margin: 1%;
+	font-family: proxima-regular;
+	text-align: center;
+}
 .box {
 	border: 1px solid black;
 	height: 100%;
-	margin-top: 5%;
+	margin-top: 2.5%;
+	margin-bottom: 2.5%;
 }
-.box .item-icon {
+.box .item {
+	height: 6vmin;
 	left: -15px;
 	background-color: #000;
 }
-.box .item-icon span {
+.box .item span {
+	-webkit-transform: translate(-50%,-50%);
+	transform: translate(-50%,-50%);
+	top: 50%;
+	left: 50%;
+	width: 100%;
+	position: absolute;
 	color: #fff;
-	font-size: 2.5em;
+	font-size: 3vmin;
 	font-family: proxima-light;
-	/*margin-top: 2%;*/
 	text-align: center;
-	/*position: absolute;*/
 }
 .box .value {
 	font-size: 4vmin;
