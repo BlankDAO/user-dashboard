c46c0adf1ee57166c68be128c3c1cd67f1c32b97
root <root@berbidvps.4.2.2.4>
2019-04-17 08:40:42 -0400

    init argon-service for vps

--- common/argon-service.py
+++ common/argon-service.py
@@ -1,10 +1,15 @@
+
+# Run with `xvfb-run python argon-service.py`
+from pyvirtualdisplay import Display
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import pymongo
 import time
 import subprocess
-
+from time import time as now
 connection_list = list()
+
 
 
 class Browser(object):
@@ -14,30 +19,34 @@ class Browser(object):
     def kill_firefox(self):
         try:
             # Kill firefox
-            p = subprocess.Popen(['killall', 'firefox'],
+            p = subprocess.Popen(['pkill', 'firefox'],
                                  stdout=subprocess.PIPE)
-            p.communicate()
+            res, error = p.communicate()
         except Exception as e:
             print(e)
 
     def open_browser(self, dao):
         try:
+            self.display = Display(visible=0, size=(1024, 768))
+            self.display.start()
             self.driver = webdriver.Firefox()
             self.driver.get("https://mainnet.aragon.org/#/{}/".format(dao))
         except Exception as e:
+            self.display.stop()
             self.driver = None
-            print(e)
+
 
     def start(self, dao):
         self.kill_firefox()
         self.open_browser(dao)
 
-        time.sleep(10)
+        time.sleep(20)
 
         if self.driver == None:
             return 'Undifind'
 
         content = self.driver.find_elements_by_class_name('item')
+        # self.driver.save_screenshot(str(now())+'.png') # check state
         for element in content:
             if element.text == 'Settings':
                 element.click()
@@ -45,15 +54,14 @@ class Browser(object):
                 for li in lis:
                     if 'FINANCE' in li.text:
                         t = li.text
-                        print(t.split('\n')[1])
-                        self.driver.close()
+                        self.display.stop()
                         return t.split('\n')[1]
-        self.driver.close()
+        self.display.stop()
         return None
 
 
 def create_connection():
-    client = pymongo.MongoClient('mongodb://localhost:27017/')
+    client = pymongo.MongoClient('mongodb://localhost:27017/', connect=False)
     connection_list.append(client)
     return client['blankdao']
 
